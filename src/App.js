5807e73caf162d4cc69ec57ffd862d4d6c089e20
shishircbs <shishir.cbs@gmail.com>
2021-03-06 22:42:09 +0545

    created sign-up page and tested the signup with email and password successfully, couldn't figure out how I am getting column view for bith sign in and sign up instead of side by side

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {Route, Switch} from 'react-router-dom';
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -27,9 +27,24 @@ constructor(){
 unsubscribeFromAuth=null;
 
 componentDidMount(){
-  this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
-    this.setState({currentUser: user});
-    console.log(user);
+  this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
+    if(userAuth){
+      const userRef= await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot(snapShot=>{
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        }
+        );
+        console.log(this.state);
+      });
+    }
+    else{
+      this.setState({currentUser: userAuth});
+    }
   });
 }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,9 +10,36 @@ const config ={
     messagingSenderId: "457970388094",
     appId: "1:457970388094:web:930fea4b7a36e671f35c2f",
     measurementId: "G-04JEDFXTZQ"
+};
+
+firebase.initializeApp(config);
+  export const createUserProfileDocument=async (userAuth, additionalData)=>{
+      if (!userAuth) return;
+
+      const userRef=firestore.doc(`users/${userAuth.uid}`);
+
+      const snapShot=await userRef.get();
+
+      if(!snapShot.exists){
+          const {displayName, email}=userAuth;
+          const createdAt= new Date();
+          try {
+              await userRef.set({
+                  displayName,
+                  email,
+                  createdAt,
+                  ...additionalData
+              });
+              
+          } catch (error) {
+              console.log('Error creating user!', error.message);
+          }
+      }
+
+      return userRef;
+
   };
 
-  firebase.initializeApp(config);
 
   export const auth=firebase.auth();
   export const firestore=firebase.firestore();
